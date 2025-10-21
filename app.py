de7d893212effc5166c28cf6387885824b73db69
1transparentpng <aidens151@nycstudents.net>
2025-10-21 09:25:50 -0400

    movie

--- app.py
+++ app.py
@@ -8,16 +8,27 @@ data = json.load(movies)
 '''for movie in data:
     print(movie["title"])'''
 ## File Two
-'''yearafter = int(input("Type a year and i'll find all movies made AFTER that year."))
+'''yearafter = int(input("Type a year and i'll find all movies made AFTER that year. >>"))
 for movie in data:
     if movie["year"] > yearafter:
         print(movie["title"])'''
 ## File Three
-yearafter = int(input("Type a year and i"))
+'''yearafter = int(input("Type a year and i'll find all movies made AFTER that year. >> "))
+yearbefore = int(input("Now, type a year AFTER the year you just gave and i'll find all movies made BEFORE that year. >> "))
+for movie in data:
+    if movie["year"] > yearafter and movie["year"] < yearbefore:
+        print(movie["title"])'''
 ## File Four
-
+yearduring = int(input("Type a year and i'll find all movies made DURING that year. >> "))
+for movie in data:
+    if movie["year"] == yearduring:
+        print(movie["title"])
 ## File Five
 
 ## File Six
-
+def filterGenre():
+    listGenre = []
+    genre = input("Please give me a Genre to filter movies by.")
+    listGenre.append(genre)
+    for movie in data  
 

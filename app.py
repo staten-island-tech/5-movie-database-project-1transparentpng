976d512040f6ce13cbe741d982d3afb46a8f0263
1transparentpng <aidens151@nycstudents.net>
2025-10-21 08:59:36 -0400

    blahblah

--- app.py
+++ app.py
@@ -4,3 +4,20 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+## File One
+'''for movie in data:
+    print(movie["title"])'''
+## File Two
+'''yearafter = int(input("Type a year and i'll find all movies made AFTER that year."))
+for movie in data:
+    if movie["year"] > yearafter:
+        print(movie["title"])'''
+## File Three
+yearafter = int(input("Type a year and i"))
+## File Four
+
+## File Five
+
+## File Six
+
+

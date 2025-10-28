5e0575ebd359d322e9bd7686ef6cd10a97d732dc
1transparentpng <aidens151@nycstudents.net>
2025-10-28 09:21:38 -0400

    File 4, 5, & 6 done

--- app.py
+++ app.py
@@ -19,16 +19,24 @@ for movie in data:
     if movie["year"] > yearafter and movie["year"] < yearbefore:
         print(movie["title"])'''
 ## File Four
-yearduring = int(input("Type a year and i'll find all movies made DURING that year. >> "))
+'''yearduring = int(input("Type a year and i'll find all movies made DURING that year. >> "))
 for movie in data:
     if movie["year"] == yearduring:
-        print(movie["title"])
+        print(movie["title"])'''
 ## File Five
-
+def findMovie():
+    search = input("What movie would you like to search for?")
+    for movie in data:
+        title = movie["title"]
+        if search in title: 
+## "in" searches for the string / variable in the other string / variable provided. this line finds if the search var (the user input, a string) is found in the title of the searched movie. If it is, it prints it
+            print(movie["title"])
+findMovie()
 ## File Six
-def filterGenre():
-    listGenre = []
+'''def filterGenre():
     genre = input("Please give me a Genre to filter movies by.")
-    listGenre.append(genre)
-    for movie in data  
-
+    if not genre:
+        print("You did not input a genre!")
+    for movie in data:
+        if genre in movie["genres"]:
+            print(movie["title"])'''
